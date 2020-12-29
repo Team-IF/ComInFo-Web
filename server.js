3978b50e824c8e96f9098784e2523894bac8a169
d!4 <dia@d4ui-MacBookPro.local>
2020-12-30 08:31:37 +0900

    디자인 변경 및 GPU정보 추가

---
+++ server.js
@@ -0,0 +1,32 @@
+const PORT = 18382
+const express = require('express')
+const app = express()
+const cors = require('cors')()
+const os = require('os')
+const gpuInfo = require('gpu-info')
+
+let cpuName = os.cpus()
+let _Gpu = []
+let gpuName = []
+
+const info = {
+    hostname: os.hostname(),
+    type: os.type(),
+    platform: os.platform(),
+    cpu: cpuName[0].model,
+    ram: Math.floor(os.totalmem),
+    uptime: Math.floor(os.uptime()/60),
+    
+}
+
+app.use(cors)
+
+app.get('/', (_, res) => {
+    gpuInfo().then(data => { _Gpu = data
+        info.gpu = _Gpu.map(x => x.Name).join('\n')
+        res.send(info)
+    })
+
+})
+
+app.listen(PORT, () => console.log('Server is now on http://localhost:' + PORT))
