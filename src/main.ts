b16e626adfc0f672a7f17e3197e23a65ab200229
Coder-Iro <Coder-Iro@teamif.io>
2021-02-02 14:35:13 +0900

    Add bootstrap@next

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import {createApp} from 'vue'
 // TypeScript error? Run VSCode command
 // TypeScript: Select TypeScript version - > Use Workspace Version
 import App from './App.vue'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import 'mdbootstrap/css/mdb.min.css'
 
 
